8d5e528a08bf776f29f5babb726996efa547ba06
LuSrackhall <3647637206@qq.com>
2025-09-24 16:46:27 +0800

    新增鸿蒙.so的构建, 最终通过linux下解压缩+成功构建.so文件。 但并未在鸿蒙项目中成功使用, 因此移除了整个鸿蒙项目的工程

---
+++ go-sdk/full/android/android.go
@@ -0,0 +1,7 @@
+package android
+
+import "test-go-sdk/full"
+
+func FullSdkRun(LogDirPath string, DataBaseDirPath string) int {
+	return full.FullSdkRun(LogDirPath, DataBaseDirPath)
+}
--- go-sdk/full/harmony/harmony.go
+++ go-sdk/full/harmony/harmony.go
@@ -1,9 +1,25 @@
+// package main
+
+// import "test-go-sdk/full"
+
+// func FullSdkRun(LogDirPath string, DataBaseDirPath string) int {
+// 	return full.FullSdkRun(LogDirPath, DataBaseDirPath)
+// }
+// func main() {}
+
 package main
 
-import "test-go-sdk/full"
+import "C"
+import "fmt"
 
-func FullSdkRun(LogDirPath string, DataBaseDirPath string) int {
-	return full.FullSdkRun(LogDirPath, DataBaseDirPath)
+//export Add
+func Add(x, y C.double) C.double {
+	return x + y
 }
 
+//export Hello
+func Hello() *C.char {
+	fmt.Println("LuSrackhall Hello")
+	return C.CString("Hello, HarmonyOS")
+}
 func main() {}
